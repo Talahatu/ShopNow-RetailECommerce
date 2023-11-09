71aaa193bf8504c376e28131a2bb9ed640688527
Talahatu <87047371+Talahatu@users.noreply.github.com>
2023-11-09 20:24:12 +0700

    fix profile responsive

--- resources/views/regular/profile.blade.php
+++ resources/views/regular/profile.blade.php
@@ -3,9 +3,29 @@
 @section('content')
     <div class="container mt-4">
         <div class="row w-100">
-            <div class="col-3 card p-3 shadow-2-strong overflow-auto" style="height:50vh;">
+            <div class="col-12 card p-3 shadow-2-strong mb-4 d-block d-md-none">
                 <!-- Tab navs -->
-                <div class="nav flex-column nav-tabs text-center" id="v-tabs-tab" role="tablist" aria-orientation="vertical">
+                <ul class="nav nav-tabs nav-justified mb-3" id="ex1" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" id="v-tabs-profile-tab" data-mdb-toggle="tab" href="#v-tabs-profile"
+                            role="tab" aria-controls="v-tabs-profile" aria-selected="true">Profile</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="v-tabs-order-tab" data-mdb-toggle="tab" href="#v-tabs-order" role="tab"
+                            aria-controls="v-tabs-order" aria-selected="false">Orders</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="v-tabs-notif-tab" data-mdb-toggle="tab" href="#v-tabs-notif" role="tab"
+                            aria-controls="v-tabs-notif" aria-selected="false">Notifications</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row w-100 ">
+            <div class="col-3 card p-3 shadow-2-strong overflow-auto d-none d-md-block" style="height:50vh;">
+                <!-- Tab navs -->
+                <div class="nav flex-column nav-tabs text-center" id="v-tabs-tab" role="tablist"
+                    aria-orientation="vertical">
                     <a class="nav-link active" id="v-tabs-profile-tab" data-mdb-toggle="tab" href="#v-tabs-profile"
                         role="tab" aria-controls="v-tabs-profile" aria-selected="true">Profile</a>
                     <a class="nav-link" id="v-tabs-order-tab" data-mdb-toggle="tab" href="#v-tabs-order" role="tab"
@@ -16,17 +36,20 @@
                 <!-- Tab navs -->
             </div>
 
-            <div class="col-9 ms-auto">
+            <div class="col-md-9 col-sm-12 ms-auto">
                 <!-- Tab content -->
                 <div class="tab-content" id="v-tabs-tabContent">
                     <div class="tab-pane fade show active" id="v-tabs-profile" role="tabpanel"
                         aria-labelledby="v-tabs-profile-tab">
+
                         @include('regular.tabs.profile-tab')
                     </div>
                     <div class="tab-pane fade" id="v-tabs-order" role="tabpanel" aria-labelledby="v-tabs-profile-tab">
+
                         @include('regular.tabs.order-tab')
                     </div>
                     <div class="tab-pane fade" id="v-tabs-notif" role="tabpanel" aria-labelledby="v-tabs-notif-tab">
+
                         @include('regular.tabs.notif-tab')
                     </div>
                 </div>
